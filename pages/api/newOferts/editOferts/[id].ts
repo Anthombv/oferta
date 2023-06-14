a2dbf40d8f30b1aa7e2147f837830df61b9a2cd5
Anthombv <anto_ny1997@hotmail.com.ar>
2023-06-14 16:47:33 -0500

    fix edit ofert

--- pages/api/newOferts/editOferts/[id].ts
+++ pages/api/newOferts/editOferts/[id].ts
@@ -42,6 +42,7 @@ const EditOfert = async (req: NextApiRequest, res: NextApiResponse) => {
   const { id } = req.query;
   const {
     cli_name,
+    cli_representante,
     cli_estadoCivil,
     cli_id,
     cli_sexo,
@@ -59,14 +60,17 @@ const EditOfert = async (req: NextApiRequest, res: NextApiResponse) => {
     cli_conyuTrab,
     cli_conyuDireccT,
     cli_conyuTelT,
+    cli_porcentaje,
     cli_descuento,
+    cli_descuentoAdd,
     cli_totalOferta,
     cli_state,
   } = req.body;
   dataBase.query(
-    "UPDATE oferta SET cli_name = ?, cli_estadoCivil = ?, cli_id = ?, cli_sexo = ?, cli_fecNac = ?, cli_telef = ?, cli_cell = ?, cli_mail = ?, cli_trabajo = ?, cli_cargoT = ?, cli_direccT = ?, cli_telefT = ?, cli_conyuName = ?, cli_conyuID = ?, cli_conyuCell = ?, cli_conyuTrab = ?, cli_conyuDireccT = ?, cli_conyuTelT = ?, cli_descuento = ?, cli_totalOferta = ?, cli_state = ? WHERE id = ?",
+    "UPDATE oferta SET cli_name = ?, cli_representante = ?, cli_estadoCivil = ?, cli_id = ?, cli_sexo = ?, cli_fecNac = ?, cli_telef = ?, cli_cell = ?, cli_mail = ?, cli_trabajo = ?, cli_cargoT = ?, cli_direccT = ?, cli_telefT = ?, cli_conyuName = ?, cli_conyuID = ?, cli_conyuCell = ?, cli_conyuTrab = ?, cli_conyuDireccT = ?, cli_conyuTelT = ?, cli_porcentaje = ?, cli_descuento = ?, cli_descuentoAdd = ?, cli_totalOferta = ?, cli_state = ? WHERE id = ?",
     [
       cli_name,
+      cli_representante,
       cli_estadoCivil,
       cli_id,
       cli_sexo,
@@ -84,7 +88,9 @@ const EditOfert = async (req: NextApiRequest, res: NextApiResponse) => {
       cli_conyuTrab,
       cli_conyuDireccT,
       cli_conyuTelT,
+      cli_porcentaje,
       cli_descuento,
+      cli_descuentoAdd,
       cli_totalOferta,
       cli_state,
       id,
@@ -92,6 +98,7 @@ const EditOfert = async (req: NextApiRequest, res: NextApiResponse) => {
   );
   return res.status(200).json({
     cli_name,
+    cli_representante,
     cli_estadoCivil,
     cli_id,
     cli_sexo,
@@ -109,7 +116,9 @@ const EditOfert = async (req: NextApiRequest, res: NextApiResponse) => {
     cli_conyuTrab,
     cli_conyuDireccT,
     cli_conyuTelT,
+    cli_porcentaje,
     cli_descuento,
+    cli_descuentoAdd,
     cli_totalOferta,
     cli_state,
     id,
--- pages/oferts/edit/[id].tsx
+++ pages/oferts/edit/[id].tsx
@@ -269,6 +269,19 @@ const EditOfert = () => {
               </div>
               <div className="flex flex-wrap">
                 <label className="block mb-2 label-size font-medium text-gray-900 dark:text-white">
+                  Nombre del representante
+                </label>
+                <input
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  type="text"
+                  name="cli_representante"
+                  id="cli_representante"
+                  value={formik.values.cli_representante}
+                  onChange={formik.handleChange}
+                />
+              </div>
+              <div className="flex flex-wrap">
+                <label className="block mb-2 label-size font-medium text-gray-900 dark:text-white">
                   Cedula o RUC
                 </label>
                 <input
